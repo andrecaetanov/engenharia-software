57d94d2c920f78071b89db93be3e858a5eef0aac
Diego Paiva <diego.paiva13@gmail.com>
2018-05-09 21:34:42 -0300

    Criação da página de seleção de dificuldade

---
+++ Jogo/src/pages/selecionar-dificuldade/selecionar-dificuldade.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { SelecionarDificuldadePage } from './selecionar-dificuldade';
+
+@NgModule({
+  declarations: [
+    SelecionarDificuldadePage,
+  ],
+  imports: [
+    IonicPageModule.forChild(SelecionarDificuldadePage),
+  ],
+})
+export class SelecionarDificuldadePageModule {}
